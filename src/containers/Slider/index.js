fbeac87a577f90cd881372c9c784e9a65d849bb9
Laurent-Daleo <137190234+Laurent-Daleo@users.noreply.github.com>
2024-11-19 15:20:00 +0100

    FIX: bug with 2 childrens have the same key, add id on array focus

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -39,9 +39,9 @@ const Slider = () => {
           </div>
           <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
+              {byDateDesc.map((newEvent, radioIdx) => (
                 <input
-                  key={`${event.id}`}
+                  key={newEvent.id}
                   type="radio"
                   name="radio-button"
                   checked={index === radioIdx}
